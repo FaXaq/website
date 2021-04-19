323e419ed3e392376a4e4a48e5b7bf91171fce56
Marin <marin.procureur@protonmail.com>
2021-04-19 20:34:17 +0200

    Add tags filters & published for articles

--- components/blog/Tags.tsx
+++ components/blog/Tags.tsx
@@ -1,14 +1,35 @@
 import React from 'react'
 
 interface TagsProps {
-  tags: string[]
+  tags: string[],
+  removable?: boolean,
+  onDelete?: (tag: string) => any | Promise<any>,
+  onClick?: (tag: string) => any | Promise<any>
 }
 
-const Tags = ({ tags }: TagsProps) => {
+const Tags = ({ tags, removable = false, onDelete, onClick }: TagsProps) => {
+  function handleOnClick (tag) {
+    if (!onClick) {
+      console.warn('no onClick handle for', tags)
+      return
+    }
+
+    return onClick(tag)
+  }
+
   return (
     <div>
       <ul className="flex py-4">
-        {tags.map(tag => <li key={tag} className="px-2 py-1 mr-2 bg-gray-500">{tag}</li>)}
+        {tags.map(tag => (
+          <li key={tag} className="flex mr-2 cursor-pointer">
+            <div className="px-2 py-1 bg-gray-500" onClick={() => handleOnClick(tag)}>
+              <span>{tag}</span>
+            </div>
+            <button onClick={() => onDelete(tag)}>
+              {removable && <span className='px-2 py-1 bg-gray-900 text-white border-none'>x</span>}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   )
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,12 +5,16 @@ import Link from 'next/link'
 import path from 'path'
 import Tags from '../../components/blog/Tags'
 import { useTranslation } from 'react-i18next'
+import { useRouter } from 'next/router'
+// eslint-disable-next-line no-unused-vars
+import { ParsedUrlQuery } from 'querystring'
 
 export interface ArticleMetaData {
   title: string
   description: string
-  creationDate: Date,
+  creationDate: string
   tags: string[]
+  published: boolean
 }
 
 interface Article {
@@ -19,11 +23,75 @@ interface Article {
 }
 
 interface BlogProps {
-  articles: Article[];
+  articles: Article[]
+  tags: string[]
 }
 
-const Blog: NextPage<BlogProps> = function ({ articles }: BlogProps) {
+function computeQueryToUrlSearchParams (query: ParsedUrlQuery): string[][] {
+  const queryParams = []
+  Object.keys(query).forEach(param => {
+    const paramValue = query[param]
+    if (paramValue instanceof Array) {
+      paramValue.forEach(value => {
+        queryParams.push([param, value])
+      })
+    } else {
+      queryParams.push([param, paramValue])
+    }
+  })
+  return queryParams
+}
+
+const Blog: NextPage<BlogProps> = function ({ articles, tags }: BlogProps) {
   const { t } = useTranslation()
+  const router = useRouter()
+
+  function removeTagFromQueryParams (tag: string) {
+    const query = router.query
+
+    /**
+     * remove tag from query param
+     */
+    if (query.tag instanceof Array) {
+      query.tag = query.tag.filter(queryTag => queryTag !== tag)
+    } else {
+      if (query.tag === tag) {
+        delete query.tag
+      }
+    }
+
+    const queryParams = computeQueryToUrlSearchParams(query)
+
+    router.push(router.route + '?' + new URLSearchParams(queryParams).toString())
+  }
+
+  function addTagToQueryParams (tag: string) {
+    const query = router.query
+
+    /**
+     * add tag to query params
+     */
+    if (query.tag) {
+      if (query.tag instanceof Array) {
+        if (query.tag.indexOf(tag) === -1) {
+          query.tag.push(tag)
+        } else {
+          return
+        }
+      } else {
+        query.tag = [
+          query.tag,
+          tag
+        ]
+      }
+    } else {
+      query.tag = [tag]
+    }
+
+    const queryParams = computeQueryToUrlSearchParams(query)
+
+    router.push(router.route + '?' + new URLSearchParams(queryParams).toString())
+  }
 
   const links = articles.map((a, i) => {
     return (
@@ -33,36 +101,71 @@ const Blog: NextPage<BlogProps> = function ({ articles }: BlogProps) {
             <div>
               <h3 className="font-mtts-title font-semibold text-2xl my-2">{a.meta.title}</h3>
               <p>{a.meta.description ? a.meta.description : '' } - <em>{t('format.date', { date: new Date(a.meta.creationDate) })}</em></p>
-              <Tags tags={a.meta.tags || []} />
             </div>
           </a>
         </Link>
+        <Tags tags={a.meta.tags || []} onClick={addTagToQueryParams} />
       </li>
     )
   })
+
   return (
     <div className="font-sans container mx-auto px-4">
-      <h1 className="text-8xl font-bold font-mtts-title">{t('blog.title')}</h1>
-      <ul className="flex flex-col">
-        {links.length > 0 ? links : 'No blog post yet...'}
-      </ul>
+      <header>
+        <h1 className="text-8xl font-bold font-mtts-title">{t('blog.title')}</h1>
+        {tags.length > 0 && (
+          <div>
+            <p>{t('blog.tagsSearch')}</p>
+            <ul>
+              <Tags tags={tags} removable onDelete={tag => removeTagFromQueryParams(tag)}/>
+            </ul>
+          </div>
+        )}
+      </header>
+      <article>
+        <ul className="flex flex-col">
+          {links.length > 0 ? links : 'No blog post here...'}
+        </ul>
+      </article>
     </div>
   )
 }
 
-Blog.getInitialProps = async (_) => {
+Blog.getInitialProps = async (context) => {
+  /** retrieve tags to filter if necessary */
+  // what is this ugly ternary
+  const tags = context.query.tag
+    ? context.query.tag instanceof Array
+      ? [...context.query.tag]
+      : [context.query.tag]
+    : []
+  /**
+   * retrieve articles
+   */
   const articles = await (async context => {
     return await Promise.all(context.keys().map(async (k) => {
       const postContent = await import(`./${k.replace('./', '')}`)
       return {
         meta: postContent.meta,
         link: `/blog/${path.basename(k.replace('./', ''), '.mdx')}`
-      }
+      } as Article
     }))
   })(require.context('./', true, /\.mdx/))
 
+  console.log(context.query)
+
+  /**
+   * remove articles that :
+   * - Are not published (except for query param notPublished)
+   * - Don't have the tags in query params
+   */
+  const filteredArticles = articles
+    .filter(article => article.meta.published || context.query.notPublished !== undefined)
+    .filter(article => tags.every(tag => article.meta.tags.includes(tag)))
+
   return {
-    articles
+    articles: filteredArticles,
+    tags
   }
 }
 
