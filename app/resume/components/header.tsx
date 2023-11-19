b6d23beebd03979e4da897c81d46a324d591bd91
Marin <marin.procureur@gmail.com>
2023-11-19 23:16:00 +0100

    Update resume before reconciliation

--- app/resume/components/header.tsx
+++ app/resume/components/header.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react'
 
-import Mountains from '../../components/images/mountains'
 import { useTranslation } from 'next-i18next'
 import LocaleSelector from '../../components/locale-selector'
 
@@ -13,7 +12,7 @@ function Header () {
       <div className="absolute top-0 right-0 z-10">
         <LocaleSelector />
       </div>
-      <div className="py-32 md:py-64 max-h-screen relative bg-dark-marine w-full">
+      <div className="py-32 relative bg-dark-marine w-full">
         <div className="container h-full px-4 lg:px-0 mx-auto flex flex-col justify-center">
           <h1 className="text-light-sand text-6xl md:text-7xl font-black">
             {t('resume.firstname')}
@@ -21,9 +20,6 @@ function Header () {
           <h2 className="text-dark-sand text-2xl md:text-3xl">
             {t('resume.workfield')}
           </h2>
-        </div>
-        <div className="absolute bottom-0 w-full">
-          <Mountains />
         </div>
       </div>
     </header>
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -3,18 +3,20 @@
 import React from 'react'
 
 import Header from './components/header'
-import Mountains from '../components/images/mountains'
 import Footer from './components/footer'
 import Content from './components/content'
+import { useTranslation } from 'react-i18next'
 
 function HomePage () {
+  const { t } = useTranslation()
+
   return (
     <div className="font-sans">
       <Header />
-      <div className="rotate-180">
-        <Mountains />
-      </div>
-      <div className="container mx-auto px-4 md:px-0">
+      <div className="container mx-auto p-4 md:px-0">
+        <p className="py-4">
+          {t('resume.description')}
+        </p>
         <Content />
       </div>
       <Footer />
