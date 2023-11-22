d32a5b8fecf7ad50d69c7efd68d962bb023c54a7
Marin <marin.procureur@gmail.com>
2023-11-22 17:19:31 +0100

    Add app & root config path

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
+const path = require('path')
+
 const withMDX = require('@next/mdx')()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions`` to include MDX files
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx']
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "root": path.resolve('./'),
+      "app": path.resolve('./app')
+    }
+
+    return config
+  }
 }
 
 module.exports = withMDX(nextConfig)
