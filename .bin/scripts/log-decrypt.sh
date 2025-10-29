056f8007f1c1b28b7e245336f1abff27ba6c5f83
Marin <marin.procureur@gmail.com>
2025-10-29 14:17:14 +0100

    fix deploy, encrypt & decrypt

--- .bin/scripts/log-decrypt.sh
+++ .bin/scripts/log-decrypt.sh
@@ -16,17 +16,6 @@ else
     shift
 fi
 
-if [[ -z "${ANSIBLE_VAULT_PASSWORD_FILE:-}" ]]; then
-  ansible_extra_opts+=("--vault-password-file" "${SCRIPT_DIR}/vault-password-show.sh")
-else
-  echo "Récupération de la passphrase depuis l'environnement variable ANSIBLE_VAULT_PASSWORD_FILE" 
-fi
-
-delete_cleartext() {
-  rm -f "$PASSPHRASE"
-}
-trap delete_cleartext EXIT
-
 rm -f /tmp/deploy.log.gpg
 
 gh run download "$RUN_ID" -n "logs-$JOB_ID" -D /tmp
--- .bin/scripts/log-encrypt.sh
+++ .bin/scripts/log-encrypt.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-if [[ -z "${ANSIBLE_VAULT_PASSWORD_FILE:-}" ]]; then
-  ansible_extra_opts+=("--vault-password-file" "${SCRIPT_DIR}/view-vault-password.sh")
-else
-  echo "Récupération de la passphrase depuis l'environnement variable ANSIBLE_VAULT_PASSWORD_FILE" 
-fi
-
 readonly PASSPHRASE="${INFRA_DIR}/public_key.asc"
 
 touch /tmp/deploy.log
