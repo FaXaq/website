f672d7c1837bfc19f2bd57db77c983cd867d3906
Marin <marin.procureur@gmail.com>
2025-12-26 20:06:12 +0100

    fix title

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -22,7 +22,7 @@ export const Route = createRootRoute({
         name: "viewport",
         content: "width=device-width, initial-scale=1",
       },
-      { title: "TanStack Start Starter" }
+      { title: "norra.fr" }
     ],
   }),
   component: RootLayout,
